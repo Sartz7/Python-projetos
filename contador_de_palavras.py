da2fc4ceb16ca7c2084dfa4f47d7ec963f97bb88
sartz7 <sartoriguilherme@hotmail.com>
2026-03-04 09:57:55 -0300

    contador de palavras ok

--- contador_de_palavras.py
+++ contador_de_palavras.py
@@ -1,13 +1,35 @@
-from contador import contar_palavras
-
-frase = input("Digite uma frase: ").strip()
-if not frase:
-    print("Erro: Nenhuma frase foi digitada.")
-else:
-    resultado = contar_palavras(frase)
+def main():
+    from contador_de_palavras import contar_palavras   
+    frase = input("Digite uma frase: ").strip()
+    if not frase:
+        print("Erro: Nenhuma frase foi digitada.")
+    else:
+        resultado = contar_palavras(frase)
     if resultado:
         print("Contagem de Palavras:")
         for palavra, quantidade in resultado.items():
             print(f"{palavra}: {quantidade}")
     else:
         print("Nenhuma palavra válida foi encontrada.")
+        
+
+def limpar_texto(texto):
+    texto = texto.lower()
+    caracteres = ".,!?:;\"'()[]{}"
+    for char in caracteres:
+        texto = texto.replace(char, "")
+    return texto
+def contar_palavras(frase):
+    frase = limpar_texto(frase)
+    if not frase.strip():
+        return {}
+    
+    palavras = frase.split()
+    contagem = {}
+    
+    for palavra in palavras:
+        contagem[palavra] = contagem.get(palavra, 0) + 1
+    
+    return contagem
+
+if __name__ == "__main__":    main()
