8baf97a1919f603f96af1393a6dbe100ce270689
sartz7 <sartoriguilherme@hotmail.com>
2026-03-12 09:08:00 -0300

    Contador de Palavras muito Grande

---
+++ Palavras_muito_grande.py
@@ -0,0 +1,14 @@
+texto = input("Digite um texto: ")
+
+palavras_longas = []
+
+for palavra in texto.split():
+    if len(palavra) > 10:
+        palavras_longas.append(palavra)
+
+if palavras_longas:
+    print("Palavras com mais de 10 caracteres:")
+    for palavra in palavras_longas:
+        print(palavra) 
+
+else:    print("Nenhuma palavra com mais de 10 caracteres foi encontrada.") 
