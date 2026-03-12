1f44961df9042fd715cb18b78fe0764d345ddbc5
sartz7 <sartoriguilherme@hotmail.com>
2026-03-12 09:34:35 -0300

    Pedra,Papel ou Tesoura

--- Pedra_papel_tesoura.py
+++ Pedra_papel_tesoura.py
@@ -5,8 +5,13 @@ def pedra_papel_tesoura():
     opcoes = ['pedra', 'papel', 'tesoura']
     escolha_computador = random.choice(opcoes)
     escolha_usuario = input("Escolha pedra, papel ou tesoura: ").lower()
+
+    if escolha_usuario not in opcoes: 
+         print("Escolha inválida!") 
+         return 
+
     print(f"Computador escolheu: {escolha_computador}")
-    
+
     if escolha_usuario == escolha_computador:
         print("Empate!")
 
